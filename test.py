1cce764e976bc1434ee71e450f4f52c9c2c7788b
沈舫涛 <sft@caiyoudata.com>
2017-12-16 22:03:18 +0800

    加入chromeoption

--- test.py
+++ test.py
@@ -4,5 +4,8 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('headless')
 chrome_options.add_argument('no-sandbox')
 driver = webdriver.Chrome(chrome_options=chrome_options)
+# driver.get("https://www.baidu.com/")
+# driver.save_screenshot(driver.title+".png")
+
 driver.get("https://www.baidu.com/")
-driver.save_screenshot(driver.title+".png")
+print driver.title
