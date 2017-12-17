bf62b32516e3719207359224d617578e9687182b
沈舫涛 <sft@caiyoudata.com>
2017-12-17 14:58:54 +0800

    测试物流

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ from selenium import webdriver
 import sys
 import io
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('headless')
-# chrome_options.add_argument('no-sandbox')
-# driver = webdriver.Chrome(chrome_options=chrome_options)
-driver = webdriver.Chrome(executable_path=r'/Users/billyshen/Documents/python_workspace/chromedriver')
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('headless')
+chrome_options.add_argument('no-sandbox')
+driver = webdriver.Chrome(chrome_options=chrome_options)
+# driver = webdriver.Chrome(executable_path=r'/Users/billyshen/Documents/python_workspace/chromedriver')
 # driver.get("https://www.baidu.com/")
 # driver.save_screenshot(driver.title+".png")
 
